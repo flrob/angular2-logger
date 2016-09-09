07468ea1fd5909997cc04116ce996a4e68c50474
Devin <me@monadone.com>
2016-09-09 09:02:46 -0600

    refactored compile (tsconfig, src/)

--- core.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @module
- * @description
- * Public API.
- */
-export * from "./app/core/level";
-export * from "./app/core/logger";
-export * from "./app/core/providers";
---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @module
+ * @description
+ * Public API.
+ */
+export * from "./level";
+export * from "./logger";
+export * from "./providers";
