a9e5f6995b3dba33f190bb32373c2a2862dcf2e2
Devin <me@monadone.com>
2016-09-09 21:00:33 -0600

    finished build refactoring, verified on other proj

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './core';
+/**
+ * @module
+ * @description
+ * Main entry point that doesn't currently export anything. Use 'angular2-logger/core'.
+ */
+export let message: string = 'should probably export cool stuff like NgModules';
